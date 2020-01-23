eaa445c478717b5ecf0091aa35db2d7733ec3693
Johnathan Leon <jaleonro@unal.edu.co>
2020-01-23 16:19:56 -0500

    someFixesAdded

--- Lab3/punto2.m
+++ Lab3/punto2.m
@@ -1,76 +1,106 @@
-#probabilidades = input("Ingrese el vector de probabilidades de cada simbolo P(x): \n");
-Px = [0.30,0.08,0.20,0.12,0.25,0.05];
-pro_sort = fliplr(sort(Px));
-long = length(pro_sort);
-a = length(pro_sort) - 2;
-matriz_pro = zeros(length(pro_sort),length(pro_sort) - 1);
-matriz_pro(1:length(pro_sort))=pro_sort;
-cont = 1 ;
-cont2 = 0;
-last_pos = length(pro_sort);
-marcados = zeros(1,last_pos - 2);
+disp("punto 2")
+warning('off', 'all');
+clc
 
-for i = 1 : 1 : a 
+#La funcion nos permite verificar si los inputs del archivo p(x) estan bien
+function y = verification_p(array)
+  sum = 0;
+  for i = array
+    sum = sum + i;
+  end
+  if sum == 1
+    y = true;
+    return
+  endif
+  y = false;
+endfunction
 
-  suma = matriz_pro(last_pos + (long * cont2 )) + matriz_pro(last_pos + (long * cont2 ) - 1);
+file_name = "P(X)Huffman.TXT";
+Px = load (file_name, "R")
+
+allow = verification_p(Px);
+
+if allow
+
+  PX_sorted = fliplr(sort(Px));
+  long = length(PX_sorted); 
+  long2 = long - 2;
+  prob_matrix = zeros(long,long - 1);
+  prob_matrix(1:long)=PX_sorted;
+  counter = 1 ;
+  counter2 = 0;
+  last_pos = long;
+  marked = zeros(1,last_pos - 2);
+
+  for i = 1 : 1 : long2 
+
+    sum = prob_matrix(last_pos + (long * counter2 )) + prob_matrix(last_pos + (long * counter2 ) - 1);
+    aux_arr = zeros(1,last_pos);
+    aux_arr = prob_matrix(1 + (long * counter2 ):(last_pos - 2) + (long * counter2 ));
+
+    aux_arr(last_pos - 1) = sum;
+    last_pos = last_pos - 1;
+    arr_final = fliplr(sort(aux_arr));
+    prob_matrix(1 + (long * counter)  : last_pos + (long * counter ) ) = arr_final;
+    pos_marc = find(arr_final==sum);
+
+      if length(pos_marc) == 1
+         marked(counter) = pos_marc(1);
+       else
+        marked(counter) = pos_marc(length(pos_marc));
+       endif
+
+    counter = counter + 1;
+    counter2 = counter2 + 1;
+  endfor
+
+  prob_matrix
+  codification = cell(1,last_pos);
   aux_arr = zeros(1,last_pos);
-  aux_arr = matriz_pro(1 + (long * cont2 ):(last_pos - 2) + (long * cont2 ));
+    codification(1) = "0";
+    codification(2) = "1";
+  codification
+  counter = counter -1 ;
+  counter3 = 2;
 
-  aux_arr(last_pos - 1) = suma;
-  last_pos = last_pos - 1;
-  arr_final = fliplr(sort(aux_arr));
-  matriz_pro(1 + (long * cont)  : last_pos + (long * cont ) ) = arr_final;
-  pos_marc = find(arr_final==suma);
 
-    if length(pos_marc) == 1
-       marcados(cont) = pos_marc(1);
+  for i = 1 : 1 : long2 
+
+    visited = marked(counter);
+    carry = codification(1,visited);
+    counter = counter - 1;
+    aux_arr = zeros(1,last_pos);
+    aux_arr = prob_matrix(((long * (counter+1)) - (long - 1) : (long*(counter+1)) - (long - (counter3+1))));
+    aux_cod = cell(1,last_pos + 1);
+   
+    
+        aux_cod(length(aux_cod) - 1) =strcat(carry,"0");
+        aux_cod(length(aux_cod) ) = strcat(carry, "1");
+    
+
+     if visited != last_pos
+        ignore = codification(visited + 1:last_pos);
+        aux_cod(last_pos - length(ignore):(visited + length(ignore)) - 1) = ignore;
+        if visited - length(ignore) > 1
+        aux_cod(1:visited - length(ignore) ) = codification(1:visited - length(ignore));
+       endif
      else
-      marcados(cont) = pos_marc(length(pos_marc));
-     endif
 
-  cont = cont + 1;
-  cont2 = cont2 + 1;
-endfor
+        aux_cod(1:visited - 1) = codification(1, visited - 1);
+    endif  
+     
+        aux_cod
+        codification = cell(1,last_pos + 1);
+        codification = aux_cod;
+     
+     counter3 = counter3 + 1;
+    last_pos = last_pos + 1;
+  endfor
 
-matriz_pro
-codificacion = cell(1,last_pos);
-aux_arr = zeros(1,last_pos);
-  codificacion(1) = "0";
-  codificacion(2) = "1";
-codificacion
-cont = cont -1 ;
-b = 2;
-
-
-for i = 1 : 1 : a 
-
-  marcado = marcados(cont);
-  carry = codificacion(1,marcado);
-  cont = cont - 1;
-  aux_arr = zeros(1,last_pos);
-  aux_arr = matriz_pro(((long * (cont+1)) - (long - 1) : (long*(cont+1)) - (long - (b+1))));
-  aux_cod = cell(1,last_pos + 1);
- 
-  
-      aux_cod(length(aux_cod) - 1) =strcat(carry,"0");
-      aux_cod(length(aux_cod) ) = strcat(carry, "1");
+  codes = aux_cod 
   
 
-   if marcado != last_pos
-      pasar = codificacion(marcado + 1:last_pos);
-      aux_cod(last_pos - length(pasar):(marcado + length(pasar)) - 1) = pasar;
-      if marcado - length(pasar) > 1
-      aux_cod(1:marcado - length(pasar) ) = codificacion(1:marcado - length(pasar));
-     endif
-   else
+else
+  disp("P(X) FORMAT: wrong input")
+endif
 
-      aux_cod(1:marcado - 1) = codificacion(1, marcado - 1);
-  endif  
-   
-      aux_cod
-      codificacion = cell(1,last_pos + 1);
-      codificacion = aux_cod;
-   
-   b = b + 1;
-  last_pos = last_pos + 1;
-endfor
